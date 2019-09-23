93be07f67e1078345e04d4bfd0fba5c809432a3f
Giovi <gioxdvi@gmail.com>
2019-09-23 20:01:43 -0300

    sdasdasdads

--- frontend/testapp/src/app/app.component.ts
+++ frontend/testapp/src/app/app.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
-
+ 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'frontend';
+ 
+  title: string;
+ 
+  constructor() {
+    this.title = 'Maiki capo asd';
+  }
 }
