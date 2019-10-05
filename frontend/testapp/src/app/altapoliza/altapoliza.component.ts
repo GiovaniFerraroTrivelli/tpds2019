73b087cccbec54896943b17be8441207d2a3b5b9
Giovi <gioxdvi@gmail.com>
2019-10-05 01:16:58 -0300

    Mejorados formularios y mejorados algunos estilos

--- frontend/testapp/src/app/altapoliza/altapoliza.component.ts
+++ frontend/testapp/src/app/altapoliza/altapoliza.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Title } from '@angular/platform-browser';
 import { HijosComponent } from '../hijos/hijos.component'
+import { NgForm } from '@angular/forms';
 
 @Component({
 	selector: 'app-altapoliza',
@@ -17,5 +18,10 @@ export class AltapolizaComponent implements OnInit {
 
 	changeTest() {
 		console.log(":v");
+	}
+
+	onSubmit(f: NgForm) {
+		console.log("aca abajo");
+		console.log(JSON.stringify(f.value));
 	}
 }
--- frontend/testapp/src/app/buscarpoliza/buscarpoliza.component.ts
+++ frontend/testapp/src/app/buscarpoliza/buscarpoliza.component.ts
@@ -1,15 +1,31 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-buscarpoliza',
   templateUrl: './buscarpoliza.component.html',
   styleUrls: ['./buscarpoliza.component.scss']
 })
+
 export class BuscarpolizaComponent implements OnInit {
 
-  constructor() { }
+	constructor() { }
 
-  ngOnInit() {
-  }
+	ngOnInit() {
+	}
 
+	isValidForm(f: NgForm)
+	{
+		for(let prop in f.value) {
+			if(f.value[prop] !== null && f.value[prop] !== '') {
+				return true;
+			}
+		}
+
+		return false;
+	}
+
+	onSubmit(f: NgForm) {
+		console.log(f.value);
+	}
 }
