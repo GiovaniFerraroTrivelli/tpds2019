ecb3be722db4ca824d65ac9b86e3876f853067d3
Miguel Storani <miguelignaciostorani@gmail.com>
2019-10-29 11:16:48 -0300

    Rest controller "/provincias" funcionando

--- backend/tpds/src/main/java/com/tpds/tpds2019/ProvinciaController.java
+++ backend/tpds/src/main/java/com/tpds/tpds2019/ProvinciaController.java
@@ -4,28 +4,47 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
 
+import org.hibernate.HibernateException;
 import org.hibernate.Session;
+import org.hibernate.SharedSessionContract;
 import org.hibernate.Transaction;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import DTOs.ProvinciaDTO;
 import dataAccess.HibernateUtil;
 import dominio.Pais;
 import dominio.Provincia;
 
+@RestController
+@CrossOrigin(origins = "http://localhost")
 public class ProvinciaController {
+	
 	@GetMapping("/provincias")
-	public List<ProvinciaDTO> getProvincias(Integer idPais){
-		Session session = HibernateUtil.getSession();
-		Transaction tx = session.beginTransaction();
-		
-		Pais pais = session.get(Pais.class, idPais);
-		Set<Provincia> provincias = pais.getProvincias();
-		List<ProvinciaDTO> result = new ArrayList<ProvinciaDTO>();
-		
-		for (Provincia provincia : provincias) {
-			result.add(provincia.getDTO());
+	public List<ProvinciaDTO> getProvincias() throws HibernateException{
+		System.out.println("lalala");
+		Pais pais;
+		try {
+			Session session = HibernateUtil.getSession();
+			Transaction tx = session.beginTransaction();
+			
+			pais = session.get(Pais.class, 1);
+			tx.commit();
+	
+			
+			Set<Provincia> provincias = pais.getProvincias();
+			List<ProvinciaDTO> result = new ArrayList<ProvinciaDTO>();
+			
+			for (Provincia provincia : provincias) {
+				result.add(provincia.getDTO());
+			}
+			return result;
+		} catch (HibernateException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			throw e;
 		}
-		return result;
 	}
+
 }
