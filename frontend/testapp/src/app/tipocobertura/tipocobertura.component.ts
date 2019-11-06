30b7dfdeabe2dbfec495537a4812825410b3f229
Giovi <gioxdvi@gmail.com>
2019-11-06 18:00:26 -0300

    ReactiveForm tipocobertuura

--- frontend/testapp/src/app/tipocobertura/tipocobertura.component.ts
+++ frontend/testapp/src/app/tipocobertura/tipocobertura.component.ts
@@ -2,32 +2,36 @@ import { Component, OnInit, Output, Input } from '@angular/core';
 import { TipoCobertura } from 'TipoCobertura';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { TipocoberturaService } from './tipocobertura.service';
+import { NgForm, FormGroup, FormControl, Validators, AbstractControl } from '@angular/forms';
 
 @Component({
-  selector: 'app-tipocobertura',
-  templateUrl: './tipocobertura.component.html',
-  styleUrls: ['./tipocobertura.component.scss']
+	selector: 'app-tipocobertura',
+	templateUrl: './tipocobertura.component.html',
+	styleUrls: ['./tipocobertura.component.scss']
 })
 export class TipocoberturaComponent implements OnInit {
-  @Input() coberturas: TipoCobertura[];
-  coberturaSeleccionada: String;
+	@Input() coberturas: TipoCobertura[];
+	coberturaSeleccionada: String;
+	selCobForm: FormGroup;
+	
+	constructor(private modalService: NgbModal, private coberturaService: TipocoberturaService) { }
+	
+	ngOnInit() {
+		this.selCobForm = new FormGroup({
+			'fechaVigencia': new FormControl(null, Validators.required),
+			'modalidadPago': new FormControl(null, Validators.required),
+		});
+	}
+	
+	openVerticallyCentered(content) {
+		this.modalService.open(content, { centered: true });
+	}
 
-  constructor(private modalService: NgbModal, private coberturaService: TipocoberturaService) { }
+	seleccionarCobertura(cobertura) {
+		this.coberturaSeleccionada = cobertura;
+	}
 
-  openVerticallyCentered(content) {
-    this.modalService.open(content, { centered: true });
-  }
-  seleccionarCobertura(cobertura){
-    this.coberturaSeleccionada = cobertura;
-  }
-  onSubmit(){
-    this.coberturas.forEach(element => {
-      if (element.nombre == this.coberturaSeleccionada){
-        console.log(element);
-      }
-    });
-  }
-  
-  ngOnInit() {
-  }
+	onSubmit(){
+		console.log(this.coberturaSeleccionada)
+	}
 }
