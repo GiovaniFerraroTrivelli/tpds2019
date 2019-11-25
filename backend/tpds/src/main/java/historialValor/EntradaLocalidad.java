7e45601527ad599ea407fdcbfb8e44dd091b8930
Francisco Busso <fbusso@frsf.utn.edu.ar>
2019-11-25 18:27:15 -0300

    Se agregan los mapeos de los historiales

---
+++ backend/tpds/src/main/java/historialValor/EntradaLocalidad.java
@@ -0,0 +1,15 @@
+package historialValor;
+
+import dominio.Localidad;
+
+public class EntradaLocalidad extends Entrada {
+	private Localidad localidad;
+
+	public Localidad getLocalidad() {
+		return localidad;
+	}
+
+	public void setLocalidad(Localidad localidad) {
+		this.localidad = localidad;
+	}
+}
