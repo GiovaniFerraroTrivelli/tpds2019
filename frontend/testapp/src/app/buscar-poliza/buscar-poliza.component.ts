00ee7abbf18bd2c70653a3f7c3db6be088207230
Giovi <gioxdvi@gmail.com>
2019-12-03 02:26:36 -0300

    POST Búsqueda de póliza

--- frontend/testapp/src/app/buscar-poliza/buscar-poliza.component.ts
+++ frontend/testapp/src/app/buscar-poliza/buscar-poliza.component.ts
@@ -1,59 +1,102 @@
+import { NgForm, FormGroup, FormControl, Validators, AbstractControl, FormBuilder, ValidatorFn, ValidationErrors } from '@angular/forms';
 import { Component, OnInit } from '@angular/core';
-import { BuscarpolizaService } from './buscarpoliza.service'
-import { Poliza } from '../poliza/poliza';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { Router } from "@angular/router";
+
+import { Poliza } from '../poliza/poliza';
 import { Cliente } from '../cliente/cliente';
+import { BusquedaPoliza } from './busquedapoliza';
+
+import { BuscarpolizaService } from './buscarpoliza.service'
+import { DialogService } from '../dialog/dialog.service';
+import { LoadingService } from '../loading/loading.service';
 import { DatashareService } from '../datashare.service'
 
 @Component({
-  selector: 'app-buscar-poliza',
-  templateUrl: './buscar-poliza.component.html',
-  styleUrls: ['./buscar-poliza.component.scss']
+	selector: 'app-buscar-poliza',
+	templateUrl: './buscar-poliza.component.html',
+	styleUrls: ['./buscar-poliza.component.scss']
 })
+
 export class BuscarPolizaComponent implements OnInit {
 
-  constructor(
-    private BuscarpolizaService: BuscarpolizaService,
-    private modalService: NgbModal,
-    private router: Router,
-    private data: DatashareService
-    ) {}
-  private idPoliza: number;
-  private polizaSeleccionada: Poliza;
-  /*private listaPolizas: Poliza[];*/
-  private listaPolizas = [
-    {idcliente:2, nroPoliza: 5, apellido: "weqew", nombre: "juan"},
-    {idcliente:2, nroPoliza: 5, apellido: "asdf", nombre: "powp"},
-    {idcliente:2, nroPoliza: 5, apellido: "erwr", nombre: "iuiu"},
-    {idcliente:2, nroPoliza: 5, apellido: "jvbn", nombre: "vbc"}
-  ];
-  private poliza: Poliza[];
-  private cliente: Cliente;
+	constructor(
+		private BuscarpolizaService: BuscarpolizaService,
+		private dialogService: DialogService,
+		private modalService: NgbModal,
+		private router: Router,
+		private loadingService: LoadingService,
+		private data: DatashareService
+	) {}
 
-  ngOnInit() {
-    this.polizaSeleccionada = null;
-  }
-  onSubmit(idPoliza: number, content){
-    this.BuscarpolizaService.postBuscarPoliza(idPoliza);
-    this.modalService.open(content, {centered: true});
-  }
-  loadPolizaSeleccionada(poliza){
-    this.polizaSeleccionada = poliza;
-  }
-  cancelSeleccionarPoliza(){
-    this.polizaSeleccionada = null;
-    this.modalService.dismissAll();
-  }
-  submitSeleccionarPoliza(){
-    if(this.polizaSeleccionada!=null){
-      this.modalService.dismissAll();
-      this.router.navigate(['/registrar-pago'])
-      this.sendData();
-    };
-  }
-  sendData(){
-    this.data.changePolizaMessage(this.polizaSeleccionada);
-    this.data.changeClienteMessage(this.cliente);
-  }
+	private busquedaPoliza: BusquedaPoliza;
+	private polizaSeleccionada: Poliza;
+	private poliza: Poliza[];
+	private cliente: Cliente;
+	private busquedaPolizaForm: FormGroup;
+
+	/*private listaPolizas: Poliza[];*/
+	
+	private listaPolizas = [
+		{ idcliente: 2, nroPoliza: 5, apellido: "weqew", nombre: "juan" },
+		{ idcliente: 2, nroPoliza: 5, apellido: "asdf", nombre: "powp" },
+		{ idcliente: 2, nroPoliza: 5, apellido: "erwr", nombre: "iuiu" },
+		{ idcliente: 2, nroPoliza: 5, apellido: "jvbn", nombre: "vbc" }
+	];
+
+	ngOnInit() {
+		this.polizaSeleccionada = null;
+
+		this.busquedaPolizaForm = new FormGroup({
+			'numeroPoliza': new FormControl(null)
+		});
+	}
+
+	onSubmit(f: NgForm, content) {
+	    this.loadingService.i();
+
+		this.BuscarpolizaService.postBuscarPoliza(f.value).subscribe(
+			data => {
+				console.log(data);
+				this.modalService.open(content, { centered: true });
+			},
+			err => {
+			    this.loadingService.d();
+
+				if(err.status == 403) {
+					this.dialogService.alert(
+						'Error al buscar una póliza',
+						err.error.mensaje
+					);
+				} else {
+					this.dialogService.alert(
+						'Error al buscar una póliza',
+						err.error.error
+					);
+				}
+			}
+		);
+	}
+
+	loadPolizaSeleccionada(poliza) {
+		this.polizaSeleccionada = poliza;
+	}
+
+	cancelSeleccionarPoliza() {
+		this.polizaSeleccionada = null;
+		this.modalService.dismissAll();
+	}
+
+	submitSeleccionarPoliza() {
+		if(this.polizaSeleccionada != null) {
+			this.modalService.dismissAll();
+			this.router.navigate(['/registrar-pago']);
+			this.sendData();
+		};
+	}
+
+	sendData() {
+		this.data.changePolizaMessage(this.polizaSeleccionada);
+		this.data.changeClienteMessage(this.cliente);
+	}
 }
--- frontend/testapp/src/app/buscar-poliza/buscarpoliza.service.ts
+++ frontend/testapp/src/app/buscar-poliza/buscarpoliza.service.ts
@@ -1,18 +1,22 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { Poliza } from '../poliza/poliza'
 import { Observable } from 'rxjs/Observable';
 
-@Injectable({
-  providedIn: 'root'
-})
-export class BuscarpolizaService {
-  private url: string;
-  constructor(private http: HttpClient) { 
-    this.url = window.location.protocol + '//' + window.location.hostname + ':8080/buscarPoliza'
-  }
+import { Poliza } from '../poliza/poliza';
+import { BusquedaPoliza } from './busquedapoliza';
 
-  public postBuscarPoliza(nroPoliza: number) {
-		return this.http.post<number>(this.url, nroPoliza);
+@Injectable({
+	providedIn: 'root'
+})
+
+export class BuscarpolizaService {
+	private url: string;
+
+	constructor(private http: HttpClient) { 
+		this.url = window.location.protocol + '//' + window.location.hostname + ':8080/buscarPoliza'
+	}
+
+	public postBuscarPoliza(busquedaPoliza: BusquedaPoliza) {
+		return this.http.post<BusquedaPoliza>(this.url, busquedaPoliza, { withCredentials: true });
 	}
 }
