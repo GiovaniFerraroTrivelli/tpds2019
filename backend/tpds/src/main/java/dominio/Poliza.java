f3e70d2076ada76f9e365e5e3dcdb724042d546f
Miguel Storani <miguelignaciostorani@gmail.com>
2019-12-04 18:15:00 -0300

    Se eliminan getters y setters innecesarios

--- backend/tpds/src/main/java/dominio/Poliza.java
+++ backend/tpds/src/main/java/dominio/Poliza.java
@@ -5,11 +5,7 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Date;
 import java.util.Set;
-
-import javax.money.MonetaryAmount;
-
 import org.javamoney.moneta.Money;
-
 import dataTransferObjects.TipoCoberturaDTO;
 import dominio.Cuota.CuotaDTO;
 import enumeradores.EstadoPoliza;
@@ -46,6 +42,88 @@ public class Poliza {
 	private Usuario usuario;
 	private BigDecimal derechoEmision;
 
+	
+@SuppressWarnings("unused")
+	public static class ResumenPoliza {
+		private String nombreTitular;
+		private String apellidoTitular;
+		private String marca;
+		private String modelo;
+		private String motor;
+		private String chasis;
+		private String patente;
+		private Date inicioVigencia;
+		private Date finVigencia;
+		private String sumaAsegurada;
+		private String premio;
+		private String derechoEmision;
+		private String descuentos;
+		private Date ultimoDiaPago;
+		private String montoTotal;
+		private String formaPago;
+		private TipoCoberturaDTO tipoCobertura;
+		private ArrayList<CuotaDTO> cuotas;
+
+		public ResumenPoliza() {
+
+		}
+
+		public ResumenPoliza(String nombreTitular, String apellidoTitular, String marca, String modelo, String motor,
+				String chasis, String patente, Date inicioVigencia, Date finVigencia, String sumaAsegurada,
+				String premio, String descuentos, Date ultimoDiaPago, String montoTotal, ArrayList<CuotaDTO> cuotas) {
+			super();
+			this.nombreTitular = nombreTitular;
+			this.apellidoTitular = apellidoTitular;
+			this.marca = marca;
+			this.modelo = modelo;
+			this.motor = motor;
+			this.chasis = chasis;
+			this.patente = patente;
+			this.inicioVigencia = inicioVigencia;
+			this.finVigencia = finVigencia;
+			this.sumaAsegurada = sumaAsegurada;
+			this.premio = premio;
+			this.ultimoDiaPago = ultimoDiaPago;
+			this.montoTotal = montoTotal;
+			this.cuotas = cuotas;
+		}
+
+		public ResumenPoliza(Poliza p) {
+
+			try {
+				this.nombreTitular = p.getCliente().getNombre();
+				this.apellidoTitular = p.getCliente().getApellido();
+				this.marca = p.getModelo().getMarca().getNombre();
+				this.modelo = p.getModelo().getNombre();
+				this.motor = p.getMotor();
+				this.chasis = p.getChasis();
+				this.patente = p.getDominio();
+				this.inicioVigencia = p.getInicioVigencia();
+				this.finVigencia = p.getFinVigencia();
+				this.sumaAsegurada = p.getModelo().getSumaAsegurada(p.getAnioFabricacion()).toString();
+				this.premio = p.getPremio().toString();
+				this.derechoEmision = (p.getDerechoEmision().toString());
+				this.ultimoDiaPago = GestorPagos.getUltimoPago(p).getFechaHora();
+				this.montoTotal = p.getMontoTotal().toString();
+				this.formaPago = p.getFormaPago().toString();
+				try {
+					this.tipoCobertura = p.getTipoCobertura().getDTO();
+				} catch (NoHayValorException e) {
+
+				}
+				ArrayList<CuotaDTO> c = new ArrayList<CuotaDTO>();
+				for (Cuota cuota : p.getCuotas()) {
+					c.add(cuota.getDTO());
+				}
+				Collections.sort(c);
+				this.cuotas = c;
+			} catch (NullPointerException e) {
+			}
+
+		}
+
+	}
+	
 	public static class PolizaDTO {
 		private Integer idPoliza;
 		private NumeroPoliza nroPoliza;
@@ -253,230 +331,6 @@ public class Poliza {
 
 		public void setFormaPago(FormaPago formaPago) {
 			this.formaPago = formaPago;
-		}
-
-	}
-
-	public static class ResumenPoliza {
-		private String nombreTitular;
-		private String apellidoTitular;
-		private String marca;
-		private String modelo;
-		private String motor;
-		private String chasis;
-		private String patente;
-		private Date inicioVigencia;
-		private Date finVigencia;
-		private String sumaAsegurada;
-		private String premio;
-		private String derechoEmision;
-		private String descuentos;
-		private Date ultimoDiaPago;
-		private String montoTotal;
-		private String formaPago;
-		private TipoCoberturaDTO tipoCobertura;
-		private ArrayList<CuotaDTO> cuotas;
-
-		public ResumenPoliza() {
-
-		}
-
-		public ResumenPoliza(String nombreTitular, String apellidoTitular, String marca, String modelo, String motor,
-				String chasis, String patente, Date inicioVigencia, Date finVigencia, String sumaAsegurada,
-				String premio, String descuentos, Date ultimoDiaPago, String montoTotal, ArrayList<CuotaDTO> cuotas) {
-			super();
-			this.nombreTitular = nombreTitular;
-			this.apellidoTitular = apellidoTitular;
-			this.marca = marca;
-			this.modelo = modelo;
-			this.motor = motor;
-			this.chasis = chasis;
-			this.patente = patente;
-			this.inicioVigencia = inicioVigencia;
-			this.finVigencia = finVigencia;
-			this.sumaAsegurada = sumaAsegurada;
-			this.premio = premio;
-			this.ultimoDiaPago = ultimoDiaPago;
-			this.montoTotal = montoTotal;
-			this.cuotas = cuotas;
-		}
-
-		public ResumenPoliza(Poliza p) {
-
-			try {
-				this.nombreTitular = p.getCliente().getNombre();
-				this.apellidoTitular = p.getCliente().getApellido();
-				this.marca = p.getModelo().getMarca().getNombre();
-				this.modelo = p.getModelo().getNombre();
-				this.motor = p.getMotor();
-				this.chasis = p.getChasis();
-				this.patente = p.getDominio();
-				this.inicioVigencia = p.getInicioVigencia();
-				this.finVigencia = p.getFinVigencia();
-				this.sumaAsegurada = p.getModelo().getSumaAsegurada(p.getAnioFabricacion()).toString();
-				this.premio = p.getPremio().toString();
-				this.derechoEmision = (p.getDerechoEmision().toString());
-				this.ultimoDiaPago = GestorPagos.getUltimoPago(p).getFechaHora();
-				this.montoTotal = p.getMontoTotal().toString();
-				this.formaPago = p.getFormaPago().toString();
-				try {
-					this.tipoCobertura = p.getTipoCobertura().getDTO();
-				} catch (NoHayValorException e) {
-
-				}
-				ArrayList<CuotaDTO> c = new ArrayList<CuotaDTO>();
-				for (Cuota cuota : p.getCuotas()) {
-					c.add(cuota.getDTO());
-				}
-				Collections.sort(c);
-				this.cuotas = c;
-			} catch (NullPointerException e) {
-			}
-
-		}
-
-		public String getNombreTitular() {
-			return nombreTitular;
-		}
-
-		public void setNombreTitular(String nombreTitular) {
-			this.nombreTitular = nombreTitular;
-		}
-
-		public String getApellidoTitular() {
-			return apellidoTitular;
-		}
-
-		public void setApellidoTitular(String apellidoTitular) {
-			this.apellidoTitular = apellidoTitular;
-		}
-
-		public String getMarca() {
-			return marca;
-		}
-
-		public void setMarca(String marca) {
-			this.marca = marca;
-		}
-
-		public String getModelo() {
-			return modelo;
-		}
-
-		public void setModelo(String modelo) {
-			this.modelo = modelo;
-		}
-
-		public String getMotor() {
-			return motor;
-		}
-
-		public void setMotor(String motor) {
-			this.motor = motor;
-		}
-
-		public String getChasis() {
-			return chasis;
-		}
-
-		public void setChasis(String chasis) {
-			this.chasis = chasis;
-		}
-
-		public String getPatente() {
-			return patente;
-		}
-
-		public void setPatente(String patente) {
-			this.patente = patente;
-		}
-
-		public Date getInicioVigencia() {
-			return inicioVigencia;
-		}
-
-		public void setInicioVigencia(Date inicioVigencia) {
-			this.inicioVigencia = inicioVigencia;
-		}
-
-		public Date getFinVigencia() {
-			return finVigencia;
-		}
-
-		public void setFinVigencia(Date finVigencia) {
-			this.finVigencia = finVigencia;
-		}
-
-		public String getSumaAsegurada() {
-			return sumaAsegurada;
-		}
-
-		public void setSumaAsegurada(String sumaAsegurada) {
-			this.sumaAsegurada = sumaAsegurada;
-		}
-
-		public String getPremio() {
-			return premio;
-		}
-
-		public void setPremio(String premio) {
-			this.premio = premio;
-		}
-
-		public String getDescuentos() {
-			return descuentos;
-		}
-
-		public void setDescuentos(String descuentos) {
-			this.descuentos = descuentos;
-		}
-
-		public Date getUltimoDiaPago() {
-			return ultimoDiaPago;
-		}
-
-		public void setUltimoDiaPago(Date ultimoDiaPago) {
-			this.ultimoDiaPago = ultimoDiaPago;
-		}
-
-		public String getMontoTotal() {
-			return montoTotal;
-		}
-
-		public void setMontoTotal(String montoTotal) {
-			this.montoTotal = montoTotal;
-		}
-
-		public ArrayList<CuotaDTO> getCuotas() {
-			return cuotas;
-		}
-
-		public void setCuotas(ArrayList<CuotaDTO> cuotas) {
-			this.cuotas = cuotas;
-		}
-
-		public String getFormaPago() {
-			return formaPago;
-		}
-
-		public void setFormaPago(String formaPago) {
-			this.formaPago = formaPago;
-		}
-
-		public String getDerechoEmision() {
-			return derechoEmision;
-		}
-
-		public void setDerechoEmision(String derechoEmision) {
-			this.derechoEmision = derechoEmision;
-		}
-
-		public TipoCoberturaDTO getTipoCobertura() {
-			return tipoCobertura;
-		}
-
-		public void setTipoCobertura(TipoCoberturaDTO tipoCobertura) {
-			this.tipoCobertura = tipoCobertura;
 		}
 
 	}
